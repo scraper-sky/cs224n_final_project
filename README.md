582e3e124a1a328dca6bf4576ce8ffe6f3d44047
scraper-sky <61133341+scraper-sky@users.noreply.github.com>
2026-02-22 21:06:34 -0800

    math olympiad pre-processing script

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Investigating trade-offs between (Mamba) State Space Model, Transformers, and Hybrid Model in specialized reasoning tasks (CS224N Custom Project)
 
+
---
+++ scripts/data_preprocessing.py
@@ -0,0 +1,15 @@
+import json
+
+def main():
+    compilation_of_all_questions = {}
+    with open("data/math_olympiad_questions.json", "r") as f:
+        list_of_questions = json.load(f)
+        for item in list_of_questions:
+            question = item["question"]
+            answer = item["final_answer"]
+            compilation_of_all_questions[question] = answer
+    return compilation_of_all_questions
+
+if __name__ == "__main__":
+    result = main()
+    print(result)
